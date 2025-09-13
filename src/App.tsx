5e33d66939adda14e203a4005786ca8c043db88f
Nazar Stepaniuk <annastepaniuk77@gmail.com>
2025-09-13 17:47:14 +0200

    feat: add task creation logic with handleAdd and blur input on submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
 import TaskInput from './components/taskInput/TaskInput';
+import Task from './model';
 
 const App = () => {
     const [inputValue, setInputValue] = useState("");
+    const [taskes, setTaskes] = useState<Task[]>([]);
+
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if(inputValue){
+            setTaskes([...taskes, {id: Date.now(), name: inputValue, isDone: false}]);
+            setInputValue("");
+        }
+    }
+
     return (
         <div className='app'>
             <h1>taskify</h1>
-            <TaskInput task={inputValue} setTask={setInputValue}/>
+            <TaskInput task={inputValue} setTask={setInputValue} handleAdd={handleAdd}/>
         </div>
     );
 }
--- src/components/taskInput/TaskInput.tsx
+++ src/components/taskInput/TaskInput.tsx
@@ -1,15 +1,23 @@
+import { useRef } from "react";
 import "./taskInput.css";
 
 interface TaskInputProps{
     task: string;
     setTask: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
 
-const TaskInput = ({task, setTask}: TaskInputProps) =>{
+const TaskInput = ({task, setTask, handleAdd}: TaskInputProps) =>{
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
     return(
-        <form className="input">
+        <form className="input" onSubmit={(e)=>{
+            handleAdd(e);
+            inputRef.current?.blur();
+        }}>
             <input type="input" className="input__box" placeholder="Enter a task" 
-                   value={task} onChange={(e)=> setTask(e.target.value)}/>
+                   ref={inputRef} value={task} onChange={(e)=> setTask(e.target.value)}/>
             <button type="submit" className="input_submit">Go</button>
         </form>
     );
